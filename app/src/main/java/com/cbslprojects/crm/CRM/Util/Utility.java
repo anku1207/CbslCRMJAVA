b045eaad80b459627512693b8e616fc586683be1
Ankit Roy <chemicalexceptions@gmail.com>
2021-07-21 11:52:24 +0530

    add

--- app/src/main/java/com/cbslprojects/crm/CRM/Util/Utility.java
+++ app/src/main/java/com/cbslprojects/crm/CRM/Util/Utility.java
@@ -50,9 +50,9 @@ public class Utility {
 
   // public static final String base_url = "http://182.73.134.116:5001/MobileService.asmx/";
 
-    public static final String domain_name ="http://testcrm.cbslprojects.com";
+    //public static final String domain_name ="http://testcrm.cbslprojects.com";
 
-  //  public static final String domain_name = "https://crm.cbslprojects.com";
+    public static final String domain_name = "https://crm.cbslprojects.com";
     public static final String base_url = domain_name + "/mobileservice.asmx/";
 
     public static boolean isNetworkAvailable(Context context) {
